57cca10da1154622cfae373130de480588cbe101
jiang <1695479391@qq.com>
2020-07-23 09:41:56 +0800

    firth

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import vue from 'vue'
+import VueRouter from 'vue-router'
+vue.use(VueRouter)
+const routers = {
+
+}
+const router = new VueRouter({
+  routers,
+  model: history
+})
+export default router
